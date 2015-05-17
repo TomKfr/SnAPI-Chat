3d9f7e00aa3a6c007aa23f0b26893448e1764b92
TomKfr <tkieffer67@gmail.com>
2015-05-17 18:03:48 +0200

    ho

--- examples/exampleFunctional.php
+++ examples/exampleFunctional.php
@@ -3,16 +3,16 @@
 require_once("../src/snapchat.php");
 
 //////////// CONFIG ////////////
-$username = ""; // Your snapchat username
-$password = ""; // Your snapchat password
-$gEmail   = ""; // Gmail account
-$gPasswd  = ""; // Gmail account password
-$debug = false; // Set this to true if you want to see all outgoing requests and responses from server
+$username = "tomkfr"; // Your snapchat username
+$password = "Thomas67x1"; // Your snapchat password
+$gEmail   = "tkieffer67@gmail.com"; // Gmail account
+$gPasswd  = "OFLjul74"; // Gmail account password
+$debug = true; // Set this to true if you want to see all outgoing requests and responses from server
 ////////////////////////////////
 
 
-$imagePath = ""; // URL or local path to a media file (image or video)
-$sendTo = array();
+// $imagePath = ""; // URL or local path to a media file (image or video)
+$sendTo = array('linepaoou');
 
 $snapchat = new Snapchat($username, $gEmail, $gPasswd, $debug);
 
@@ -26,13 +26,13 @@ echo "My friends: ";
 print_r($friends);
 
 // Send snap adding text to your image and 10 seconds
-$snapchat->send($imagePath, $sendTo, "this is a test :D", 10);
+// $snapchat->send($imagePath, $sendTo, "this is a test :D", 10);
 
 // Set a story
 // $snapchat->setStory($imagePath);
 
 // Set a story adding text to the image and 5 seconds
-$snapchat->setStory($imagePath, 5, "This is my story");
+// $snapchat->setStory($imagePath, 5, "This is my story");
 
 
 // Get snaps data (Without storing them)
@@ -42,7 +42,7 @@ $snapchat->setStory($imagePath, 5, "This is my story");
 $snapchat->getSnaps(true);
 
 // Download stories for a specific username
-$snapchat->getStoriesByUsername("homie", true);
+// $snapchat->getStoriesByUsername("homie", true);
 
 // Send chat message to "username"
 $snapchat->sendMessage("username", "hello from Snap-API!");
