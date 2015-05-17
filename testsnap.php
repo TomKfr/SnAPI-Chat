0a10fdf09004cfda7b9bb77e5c0d61201c7aa470
TomKfr <tkieffer67@gmail.com>
2015-05-17 18:08:10 +0200

    test

--- testsnap.php
+++ testsnap.php
@@ -1,22 +1,24 @@
 <?php
 // testsnap.php fichier de test de l'api SnapChat
 
-require_once '/src/snapchat.php';
+require_once './src/snapchat.php';
 
+//////////// CONFIG ////////////
 $username = "tomkfr"; // Your snapchat username
 $password = "Thomas67x1"; // Your snapchat password
-$gEmail   = ""; // Gmail account
-$gPasswd  = ""; // Gmail account password
-$debug = false; // Set this to true if you want to see all outgoing requests and responses from server
+$gEmail   = "tkieffer67@gmail.com"; // Gmail account
+$gPasswd  = "OFLjul74"; // Gmail account password
+$debug = true; // Set this to true if you want to see all outgoing requests and responses from server
+////////////////////////////////
 
 $snapchat = new Snapchat($username, $gEmail, $gPasswd, $debug);
 
 //Login to Snapchat with your username and password
-$friends = $snapchat->getFriends();
-
 $snapchat->login($password);
 
-$snapchat->addFriend('linepaoou');
+$friends = $snapchat->getFriends();
+
+// $snapchat->addFriend('linepaoou');
 
 $message = 'Coucou Paupau !';
 $to = 'linepaoou';
